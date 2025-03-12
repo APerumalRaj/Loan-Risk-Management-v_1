f632e92c60d35a524f10ba7a06bad550917d6cbb
Perumal Raj A <112820701+APerumalRaj@users.noreply.github.com>
2025-03-12 22:59:23 +0530

    Add files via upload

---
+++ Loan_r_m.v.1/Loan_r_m.v.1/src/main/java/com/example/Loan_r_m/v1/Application.java
@@ -0,0 +1,13 @@
+package com.example.Loan_r_m.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Loan_r_m.v.1/Loan_r_m.v.1/src/test/java/com/example/Loan_r_m/v1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loan_r_m.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
